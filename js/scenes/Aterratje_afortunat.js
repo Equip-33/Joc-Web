8e20bdad6106f43f9dade09933f2b5bc6528b3a6
Raúl Jiménez <u1981148@campus.udg.edu>
2023-05-22 13:39:12 +0200

    Fix text punts

--- js/scenes/Aterratje_afortunat.js
+++ js/scenes/Aterratje_afortunat.js
@@ -71,8 +71,8 @@ class GameScene extends Phaser.Scene {
     handleCollisionB(plataforma, bomba) {
         bomba.destroy();
         this.puntuacion -= 20; // Incrementa la puntuación por cada globo guardado
-        this.puntuacionText.setText('Puntuación: ' + this.puntuacion); // Actualiza el texto de puntuación
         if(this.puntuacion<0) this.puntuacion=0;
+        this.puntuacionText.setText('Puntuación: ' + this.puntuacion); // Actualiza el texto de puntuación
         // Aquí puedes agregar la lógica adicional que desees cuando la plataforma colisione con una bomba
     }
 
