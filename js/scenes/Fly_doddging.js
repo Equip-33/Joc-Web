66eb8db93b92b5fea674e8878d2cdc3ceb39e84b
Raúl Jiménez <u1981148@campus.udg.edu>
2023-05-23 23:12:08 +0200

    fix

--- js/scenes/Fly_doddging.js
+++ js/scenes/Fly_doddging.js
@@ -54,7 +54,7 @@ class GameScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('bomba', '../resources/bomba.png');
+		this.load.image('bomba', '../resources/bomba.png');
         this.load.image('globo', '../resources/globo.png');
 		this.load.image('fitxer', '../resources/Fitxer.png');
     }
