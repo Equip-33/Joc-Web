37b568fd3405c142c88454d0d587cb342f78c584
claraconstans <u1979264@campus.udg.edu>
2023-05-24 12:24:25 +0200

    Update Aterratje_afortunat.js

--- js/scenes/Aterratje_afortunat.js
+++ js/scenes/Aterratje_afortunat.js
@@ -20,8 +20,8 @@ var MiniScreenScene = new Phaser.Class({
         this.textIn.setScale(0.4);
         const buttonExit = this.add.sprite(this.cameras.main.centerX , this.cameras.main.height - 300, 'boton');
         buttonExit.text
-		buttonExit.scaleX = .5;
-		buttonExit.scaleY = .5;
+		buttonExit.scaleX = .4;
+		buttonExit.scaleY = .4;
         buttonExit.setInteractive();
         buttonExit.on('pointerdown', () => {
 			loadpage("../");
