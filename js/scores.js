4cd67daf7c1d6f0562c88841d724e14b430052f7
Raúl Jiménez <u1981148@campus.udg.edu>
2023-05-24 16:48:05 +0200

    Fix problems

--- js/scores.js
+++ js/scores.js
@@ -1,25 +1,27 @@
-var load_objS = function(){
-	var vue_instance = new Vue({
-		el: "#scores_id",
-		data: {
-			scoresAA: [],
-			scoresFD: []
-		},
-		created: function(){
-			let arrayScoresAA = [];
-			if(localStorage.scoresAA){
-				arrayScoresAA = JSON.parse(localStorage.scoresAA);
-				if(!Array.isArray(arrayScoresAA)) arrayScoresAA = [];
-			}
-			this.scoresAA = arrayScoresAA;
-			let arrayScoresFD = [];
-			if(localStorage.scoresFD){
-				arrayScoresFD = JSON.parse(localStorage.scoresFD);
-				if(!Array.isArray(arrayScoresFD)) arrayScoresFD = [];
-			}
-			this.scoresFD = arrayScoresFD;
-		},
-	});
-	return {}; 
-}();
+var vue_instance = new Vue({
+	el: "#scores_id",
+	data: {
+		scoresAA: [],
+		scoresFD: []
+	},
+	created: function(){
+		let arrayScoresAA = [];
+		if(localStorage.scoresAA){
+			arrayScoresAA = JSON.parse(localStorage.scoresAA);
+			if(!Array.isArray(arrayScoresAA)) arrayScoresAA = [];
+		}
+		this.scoresAA = arrayScoresAA;
+		let arrayScoresFD = [];
+		if(localStorage.scoresFD){
+			arrayScoresFD = JSON.parse(localStorage.scoresFD);
+			if(!Array.isArray(arrayScoresFD)) arrayScoresFD = [];
+		}
+		this.scoresFD = arrayScoresFD;
+	},
+	methods: {
+		exit() {
+			loadpage("../");
+		}
+	}
+});
 
